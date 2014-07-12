2aa5fb9421dfa710836fd90f182448bd9fd9e548
nikolas <nikolas@nikolas-netbook>
2014-07-12 15:47:51 +0300

    Players changed to GameLogic, which knows the players(list) also some functions added

--- src/finalproject/server/GameLogic.java
+++ src/finalproject/server/GameLogic.java
@@ -1,11 +1,24 @@
 package finalproject.server;
 
+import java.net.Socket;
+import java.util.Random;
+
 public class GameLogic {
     
     private int state;
-    
+    private int playerTurn;
+    private Players ppl;
+    private Random rand;
     GameLogic(Players ppl){
-        //Does something.. also starts the game
+        this.ppl = ppl;
+        this.state = 0;
     }
     
+    public boolean addPlayer(Socket sok,String name,int color){
+        return ppl.addPlayer(sok, name, color);
+    }
+    
+    public int findFirstPlayer(){
+        return rand.nextInt(ppl.sizePlayer()+1);
+    }
 }
